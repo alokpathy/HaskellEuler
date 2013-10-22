3212d8c0340ad09ec1a67db532811410643ace0f
Alok Tripathy <kullalok@gmail.com>
2013-10-22 09:21:58 -0400

    Got problem1

--- Problem1.hs
+++ Problem1.hs
@@ -1,2 +1,2 @@
 problem1 :: (Integral a) => a -> a
-problem1 x = (sum [y | y <- [1..x], y `mod` 3 == 0 ||y `mod` 5 == 0]) -  1000
+problem1 = sum [y | y <- [1..999], y `mod` 3 == 0 ||y `mod` 5 == 0]
